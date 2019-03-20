92e7e093da940819e5ad5b2ba3796fa163ef379f
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-03-20 14:59:31 +0000

    Files:client/src/app/app.d.ts, client/src/application.json

--- client/src/app/app.d.ts
+++ client/src/app/app.d.ts
@@ -20,9 +20,14 @@ declare module 'app/menu/menu' {
 declare module 'app/app.screens' {
 	/***  Generated file, do not change.  */
 	import { MenuComponent } from 'app/menu/menu';
+	import { test_PhonePortrait } from 'pages/test/PhonePortrait/test';
 	export class Screens {
 	    static declarations: (typeof MenuComponent)[];
-	    static mapping: {};
+	    static mapping: {
+	        'test': {
+	            PhonePortrait: typeof test_PhonePortrait;
+	        };
+	    };
 	}
 
 }
